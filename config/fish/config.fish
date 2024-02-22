d8d6712317fbefc8f3e434e5250367ac8edf67cc
dx3906 <21009201351@stu.xidian.edu.cn>
2024-02-22 13:58:50 +0800

    update

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -3,6 +3,4 @@ if status is-interactive
     set -x LANG zh_CN.UTF-8
     set -x LANGUAGE zh_CN:en_US
     set -g fish_greeting
-    alias wget='wget --hsts-file="$XDG_DATA_HOME/wget-hsts"'
-    alias gdb='gdb -n -x $XDG_CONFIG_HOME/gdb/init'
 end
